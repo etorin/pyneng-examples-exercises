94bf279eb992bc0816d7dd942ce219342c9381a5
Evgeny Torin <31047184+etorin@users.noreply.github.com>
2020-09-15 23:20:39 +0300

    12 module was finished

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -15,3 +15,29 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
 
+import subprocess
+from typing import List
+
+def ping_ip_addresses(ip_address_list, count=1):
+    '''
+    Ping IP address and return tuple
+    '''
+    available_list = List['str'] = []
+    unavailable_list = List['str'] = []
+    for ip in ip_address_list:
+        reply = subprocess.run('ping -c {} -n {}'
+                               .format(count, ip),
+                               shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+#                               encoding='utf-8')
+        if reply.returncode == 0:
+#            print(ip, 'OK')
+            available_list.append(ip)
+        else:
+#            print(ip, 'Not OK')
+            unavailable_list.append(ip)
+    return (available_list, unavailable_list)
+
+if __name__ == '__main__':
+    print(ping_ip_addresses(['192.168.109.141', '8.8.8.8', '192.168.199.1', '1.1.1.1', '2.2.2.2']))
--- exercises/12_useful_modules/task_12_2.py
+++ exercises/12_useful_modules/task_12_2.py
@@ -31,3 +31,79 @@
 
 '''
 
+import ipaddress
+
+def if_ip(ip_or_not):
+    '''
+    Функция принимает на вход что-то и проверяет,
+    ip это или нет. Возвращает объект ip в случае успеха
+    Тут можно было возвращать строку с IP.
+    '''
+    try:
+        ip = ipaddress.ip_network(ip_or_not)
+        return ip
+    except ValueError:
+        return False
+
+def convert_ranges_to_ip_list(incoming_list):
+    '''
+    Конвертирует список IP-адресов в разных форматах в список, 
+    где каждый IP-адрес указан отдельно.
+    '''
+    # Пустой список
+    list_of_full_ips = []
+    # Цикл по входному листу
+    for ip_or_range in incoming_list:
+        # Если указан диапазон
+        if '-' in ip_or_range:
+            # Часть IP до тире
+            ip_obj = if_ip(ip_or_range.split('-')[0])
+            # Часть IP после тире
+            ip_second_obj = if_ip(ip_or_range.split('-')[1])
+            # И если обе части являются IP адресами
+            if ip_obj and ip_second_obj:
+                # то тут диапазон между 2-мя IP
+                # нужен список октетов первого и второго IP
+                ip_obj_list = ip_obj.network_address.compressed.split('.')
+                ip_second_obj_list = ip_second_obj.network_address.compressed.split('.')
+                # Тут бесполезная проверка на совпадение первых 3х октетов
+                if all(ip_obj_list[i] == ip_second_obj_list[i] for i in range(0, 3)):
+                    # если совпали - проходим по диапазону между начальным и конечным значением 4-го октета
+                    for i in range(int(ip_obj_list[3]), int(ip_second_obj_list[3])+1):
+                        # тут костыль для первых трех
+                        list_of_octets = [ip_obj_list[y] for y in range(0,3)]
+                        # тут не смог разом получить и join всех четырех, поэтому так
+                        list_of_octets.append(str(i))
+                        # в итоге в цикле добавляем все IP
+                        list_of_full_ips.append('.'.join(list_of_octets))
+                # Иначе десятичная математика не подойдет
+                else:
+                   print('More sophisticated math')
+            # А если первый IP, а второй нет
+            elif ip_obj and not ip_second_obj:
+                # Часть IP до тире и нужен список октетов
+                ip_obj = if_ip(ip_or_range.split('-')[0])
+                ip_obj_list = ip_obj.network_address.compressed.split('.')
+                # Часть НЕ IP после тире
+                last_octet = int(ip_or_range.split('-')[1])
+                # Тут проходим по диапазону между начальным и конечным значением 4-го октета
+                for i in range(int(ip_obj_list[3]), last_octet + 1):
+                    # тут тот же костыль
+                    list_of_octets = [ip_obj_list[y] for y in range(0, 3)]
+                    list_of_octets.append(str(i))
+                    list_of_full_ips.append('.'.join(list_of_octets))
+        else:
+            # если не диапазон вообще
+            print('This {} is not range'.format(ip_or_range))
+            # проверяем что ip и добавляем в список общий
+            if if_ip(ip_or_range):
+                list_of_full_ips.append(ip_or_range)
+            # иначе какая-то непонятная переменная, ну ее
+            else:
+                print('Strange value {}'.format(ip_or_range))
+
+    return list_of_full_ips
+
+if __name__ == '__main__':
+    list_of_ips_and_ranges = ['8.8.4.4', '1.1.1.1-3', '172.21.41.128-172.21.41.132']
+    print(convert_ranges_to_ip_list(list_of_ips_and_ranges))
